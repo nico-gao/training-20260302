e18ac9a67052270b1773784e06230d0f9a91214d
nico-gao <jtgao99@gmail.com>
2026-03-03 16:17:45 -0500

    Initial commit

---
+++ html/index.js
@@ -0,0 +1,7 @@
+console.log("hello");
+
+// const button = document.querySelector("#btn-1");
+// console.dir(button);
+
+// console.log(window);
+console.log(global);
---
+++ note.js
@@ -0,0 +1,43 @@
+/**
+ * DNS lookup
+ * http
+ *  request
+ *  response
+ *
+ * CRP - critical rendering path
+ *
+ * cookie vs session storage vs local storage
+ *
+ * GET: get data from server
+ * POST: send data to server
+ * PUT/PATCH: update the entire data object/ update partial data
+ * DELETE
+ *
+ * status code
+ *
+ * 100 - informational response
+ * 200 - success
+ * 300 - redirection
+ * 400 - client error
+ *  400 - bad request
+ *  401 - unauthorized
+ *  403 - forbidden
+ *  404 - not found
+ * 500 - server error
+ *
+ * GET is idempotent
+ *
+ *
+ * DOM
+ * describe the structure of the document
+ * manipulation, traversal, handle events
+ *
+ * ECMAScript - syntax standard
+ * ES6
+ *
+ * Javascript - implementation of the standard
+ *
+ * JS runtime
+ * browser - ECMAscript + WebAPI (DOM api, network, storage, window)
+ * nodejs - ECMAScript + NodeAPI (file system, path, network(creating server), process, global)
+ */
